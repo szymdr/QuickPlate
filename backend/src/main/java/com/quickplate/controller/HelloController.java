66390495220146501d47b7f5bb548d3de895af77
Szymon Dral <szymon.dral@gmail.com>
2025-03-06 16:58:18 +0100

    minor fixes

--- backend/src/main/java/com/quickplate/controller/HelloController.java
+++ backend/src/main/java/com/quickplate/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.quickplate.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
