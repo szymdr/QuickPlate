7997bef4d0845f7546196178fb9e284871c7a9ad
Szymon Dral <szymon.dral@gmail.com>
2025-03-20 18:13:37 +0100

    feat(api): add DishController with CRUD operations for dish management

--- backend/src/main/java/com/quickplate/controller/UserController.java
+++ backend/src/main/java/com/quickplate/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.quickplate.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
