3591baf4996c32b47da5acbf7938dce7a01ad057
Szymon Dral <szymon.dral@gmail.com>
2025-05-07 23:39:34 +0200

    docs(api): add OpenAPI documentation configuration and update application properties

--- backend/src/main/java/com/quickplate/QuickPlateApplication.java
+++ backend/src/main/java/com/quickplate/QuickPlateApplication.java
@@ -1,9 +1,17 @@
 package com.quickplate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@OpenAPIDefinition(
+  info = @Info(
+    title = "QuickPlate REST API",
+    version = "v1",
+    description = "Dokumentacja endpointów użytkowników, restauracji, zamówień itd."
+  )
+)
 @SpringBootApplication
 public class QuickPlateApplication {
     public static void main(String[] args) {
